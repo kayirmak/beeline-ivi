b309c0e550e03a671c5a10be6c4f437d5e474952
Kairat <dzhumabaev.kai@gmail.com>
2022-12-02 09:07:36 +0600

    implementation burger-menu

--- script.js
+++ script.js
@@ -1,8 +1,24 @@
 document.addEventListener('scroll', () => {
     const header = document.getElementById('header')
-    if (window.pageYOffset > 0) {
-        header.classList.add('header')
-    }
-    
-    else header.classList.remove('header')
+    window.pageYOffset > 0 ? header.classList.add('header') : header.classList.remove('header')
+})
+
+document.querySelector('.mobile-menu__items').addEventListener('click', (e) => {
+    const target = e.target.closest('.mobile-menu__item')
+    const list = target.querySelector('.mobile-sub-menu__items')
+    list ? list.classList.toggle('mobile-sub-menu__show') : ''
+
+    e.target.classList[0] === 'mobile-menu__item-img' ? e.target.classList.toggle('rotate-icon') : ''
+})
+
+document.querySelector('.burger-menu').addEventListener('click', () => {
+    const mobileMenu = document.querySelector('.mobile-menu')
+    mobileMenu.classList.remove('menu-hidden')
+    mobileMenu.classList.add('menu-show')
+})
+
+document.querySelector('.close-menu').addEventListener('click', () => {
+    const mobileMenu = document.querySelector('.mobile-menu')
+    mobileMenu.classList.remove('menu-show')
+    mobileMenu.classList.add('menu-hidden')
 })
